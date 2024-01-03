9e06b5986853e63fdbc7b6566c28732e8780ebf8
Pramod Chunduri <pramodc@gatech.edu>
2024-01-03 16:20:36 -0500

    migrate to openai 1.x and improve function calling prompts

--- subquestion_generator.py
+++ subquestion_generator.py
@@ -1,4 +1,3 @@
-
 import json
 from typing import List
 from enum import Enum
@@ -16,12 +15,18 @@ from openai_utils import llm_call
 #                  The subquestions should be complete questions that can be answered by a single function and a single data source.
 #                  """
 
+# DEFAULT_SUBQUESTION_GENERATOR_PROMPT = """
+#     You are an AI assistant that specializes in breaking down complex questions into simpler, manageable sub-questions.
+#     When presented with a complex user question, your role is to generate a list of sub-questions that, when answered, will comprehensively address the original query.
+#     You have at your disposal a pre-defined set of functions and data sources to utilize in answering each sub-question.
+#     If a user question is straightforward, your task is to return the original question, identifying the appropriate function and data source to use for its solution.
+#     Please remember that you are limited to the provided functions and data sources, and that each sub-question should be a full question that can be answered using a single function and a single data source.
+# """
+
 DEFAULT_SUBQUESTION_GENERATOR_PROMPT = """
     You are an AI assistant that specializes in breaking down complex questions into simpler, manageable sub-questions.
-    When presented with a complex user question, your role is to generate a list of sub-questions that, when answered, will comprehensively address the original query.
-    You have at your disposal a pre-defined set of functions and data sources to utilize in answering each sub-question.
-    If a user question is straightforward, your task is to return the original question, identifying the appropriate function and data source to use for its solution.
-    Please remember that you are limited to the provided functions and data sources, and that each sub-question should be a full question that can be answered using a single function and a single data source.
+    You have at your disposal a pre-defined set of functions and files to utilize in answering each sub-question.
+    Please remember that your output should only contain the provided function names and file names, and that each sub-question should be a full question that can be answered using a single function and a single file.
 """
 
 DEFAULT_USER_TASK = ""
@@ -32,46 +37,110 @@ class FunctionEnum(str, Enum):
     Use vector_retrieval for fact-based questions such as demographics, sports, arts and culture, etc.
     Use llm_retrieval for summarization questions, such as positive aspects, history, etc.
     """
+
     VECTOR_RETRIEVAL = "vector_retrieval"
     LLM_RETRIEVAL = "llm_retrieval"
 
 
-def generate_subquestions(question,
-                          data_sources: List[str] = None,
-                          system_prompt=DEFAULT_SUBQUESTION_GENERATOR_PROMPT,
-                          user_task=DEFAULT_USER_TASK,
-                          llm_model="gpt-4-0613",
-                          ):
+def generate_subquestions(
+    question,
+    file_names: List[str] = None,
+    system_prompt=DEFAULT_SUBQUESTION_GENERATOR_PROMPT,
+    user_task=DEFAULT_USER_TASK,
+    llm_model="gpt-4-0613",
+):
     """Generates a list of subquestions from a user question along with the
     data source and the function to use to answer the question using OpenAI LLM.
     """
-    DataSourceEnum = Enum('DataSourceEnum', {x.upper(): x for x in data_sources})
-    DataSourceEnum.__doc__ = "The data source to use to answer the corresponding subquestion"
+    FilenameEnum = Enum("DataSourceEnum", {x.upper(): x for x in file_names})
+    FilenameEnum.__doc__ = f"The names of the file to use to answer the corresponding subquestion - e.g. {file_names[0]}"
 
     # Create pydantic class dynamically
     QuestionBundle = create_model(
-        'QuestionBundle',
-        question=(str, Field(None, description="The subquestion extracted from the user's question")),
+        "QuestionBundle",
+        question=(
+            str,
+            Field(
+                None, description="The subquestion extracted from the user's question"
+            ),
+        ),
         function=(FunctionEnum, Field(None)),
-        data_source=(DataSourceEnum, Field(None))
+        file_name=(FilenameEnum, Field(None)),
     )
 
     SubQuestionBundleList = create_model(
-        'SubQuestionBundleList',
-        subquestion_bundle_list=(List[QuestionBundle],
-                                 Field(None, description="A list of subquestions - each item in the list contains a question, a function, and a data source")),
-        __base__=OpenAISchema
+        "SubQuestionBundleList",
+        subquestion_bundle_list=(
+            List[QuestionBundle],
+            Field(
+                None,
+                description="A list of subquestions - each item in the list contains a question, a function, and a file name",
+            ),
+        ),
+        __base__=OpenAISchema,
     )
 
     user_prompt = f"{user_task}\n Here is the user question: {question}"
 
-    response, cost = llm_call(model=llm_model,
-                              function_schema=[SubQuestionBundleList.openai_schema],
-                              output_schema={"name": SubQuestionBundleList.openai_schema["name"]},
-                              system_prompt=system_prompt,
-                              user_prompt=user_prompt)
+    few_shot_examples = [
+        {
+            "role": "user",
+            "content": "Compare the population of Atlanta and Toronto?",
+        },
+        {
+            "role": "function",
+            "name": "SubQuestionBundleList",
+            "content": """
+            {
+                "subquestion_bundle_list": [
+                    {
+                        "question": "What is the population of Atlanta?",
+                        "function": "vector_retrieval",
+                        "file_name": "Atlanta"
+                    },
+                    {
+                        "question": "What is the population of Toronto?"
+                        "function": "vector_retrieval",
+                        "file_name": "Toronto"
+                    }
+                ]
+            }""",
+        },
+        {
+            "role": "user",
+            "content": "Summarize the history of Chicago and Houston.",
+        },
+        {
+            "role": "function",
+            "name": "SubQuestionBundleList",
+            "content": """
+            {
+                "subquestion_bundle_list": [
+                    {
+                        "question": "What is the history of Chicago?",
+                        "function": "llm_retrieval",
+                        "file_name": "Chicago"
+                    },
+                    {
+                        "question": "What is the history of Houston?",
+                        "function": "llm_retrieval",
+                        "file_name": "Houston"
+                    }
+                ]
+            }""",
+        },
+    ]
 
-    subquestions_list = json.loads(response["choices"][0]["message"]["function_call"]["arguments"])
+    response, cost = llm_call(
+        model=llm_model,
+        function_schema=[SubQuestionBundleList.openai_schema],
+        output_schema={"name": SubQuestionBundleList.openai_schema["name"]},
+        system_prompt=system_prompt,
+        user_prompt=user_prompt,
+        few_shot_examples=few_shot_examples,
+    )
+
+    subquestions_list = json.loads(response.choices[0].message.function_call.arguments)
 
     subquestions_pydantic_obj = SubQuestionBundleList(**subquestions_list)
     subquestions_list = subquestions_pydantic_obj.subquestion_bundle_list
